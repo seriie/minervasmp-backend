e9bc2fb7e7053b6c7aac0190505fabc401a89e0e
zee <mohammadzidane058@gmail.com>
2026-03-13 11:31:17 +0700

    switching to paypal live

--- src/config/paypal.js
+++ src/config/paypal.js
@@ -2,7 +2,7 @@ import paypal from '@paypal/checkout-server-sdk';
 import dotenv from 'dotenv';
 dotenv.config({ override: true });
 
-const environment = new paypal.core.SandboxEnvironment(
+const environment = new paypal.core.LiveEnvironment(
     process.env.PAYPAL_CLIENT_ID,
     process.env.PAYPAL_CLIENT_SECRET
 );
